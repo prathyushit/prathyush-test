b31a210bdd772382439f15095f3287e84266bd89
prathyushit <131624513+prathyushit@users.noreply.github.com>
2024-12-17 16:14:39 -0500

    Update http.js

--- src/config/http.js
+++ src/config/http.js
@@ -6,7 +6,7 @@ import { performLogout } from "./authUtils";
 // const BASE_URL = "https://6i4wiooc74.execute-api.us-east-1.amazonaws.com/";
 // const BASE_URL = "https://amekkdkvdl.execute-api.us-east-1.amazonaws.com/";
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
-export default BASE_URL;
+//export default BASE_URL;
 
 let axiosInstance;
 axiosInstance = axios.create({
