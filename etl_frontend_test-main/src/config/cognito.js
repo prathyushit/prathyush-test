3da5e3dacc88956f6efa245bdb3ebcad47b0f796
prathyush <prathyush.it@gmail.com>
2024-12-16 23:25:26 -0500

    Remove all existing content

--- etl_frontend_test-main/src/config/cognito.js
+++
@@ -1,4 +0,0 @@
-export const poolData = {
-  UserPoolId: process.env.REACT_APP_USER_POOL_ID,
-  ClientId: process.env.REACT_APP_CLIENT_ID,
-}
--- etl_frontend_test-main/src/store/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { rootStore } from "./rootStore";
-
-export const StoresContext = React.createContext(rootStore);
-
-export const useStores = () => React.useContext(StoresContext);
